de330ef508b0ec1be82ebefabd775f43292673d4
RohitP <rohitpatil7424@gmail.com>
2025-05-15 12:18:50 +0530

    mail

--- send-email.js
+++ send-email.js
@@ -19,12 +19,15 @@ if (fs.existsSync(reportPath)) {
 }
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
   port: 587,
   secure: false, // use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
